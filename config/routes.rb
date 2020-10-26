0afb7a554f65ab71e6d2d3336b56cc86aad413f6
Max Lestage <maxlestage@icloud.com>
2020-10-26 15:04:01 +0100

    db heroku

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,7 @@
 Rails.application.routes.draw do
   root 'welcome#home'
   get '/salut(/:name)', to: 'welcome#salut', as: 'salut' 
-  #resources :posts
-
-    ### posts ###
-    
-      get    '/posts'          => 'posts#index'
-      get    '/posts/new'      => 'posts#new'
-      get    '/posts/:id'      => 'posts#show'
-      post   '/posts'          => 'posts#create'
-      get    '/posts/:id/edit' => 'posts#edit'
-      patch  '/posts/:id'      => 'posts#update'
-      put    '/posts/:id'      => 'posts#update'
-      delete '/posts/:id'      => 'posts#destroy'
-  
+  resources :posts
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
