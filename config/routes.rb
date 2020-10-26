0cba18d75f7f4ac94c5789b0fa1b3b030600b9df
Max Lestage <maxlestage@icloud.com>
2020-10-26 14:27:45 +0100

    add crud

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
   get '/salut(/:name)', to: 'welcome#salut', as: 'salut' 
   resources :posts
 
+    ### posts ###
+    
+      get    '/posts'          => 'posts#index'
+      get    '/posts/new'      => 'posts#new'
+      get    '/posts/:id'      => 'posts#show'
+      post   '/posts'          => 'posts#create'
+      get    '/posts/:id/edit' => 'posts#edit'
+      patch  '/posts/:id'      => 'posts#update'
+      put    '/posts/:id'      => 'posts#update'
+      delete '/posts/:id'      => 'posts#destroy'
+  
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
